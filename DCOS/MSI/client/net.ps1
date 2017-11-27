fe85692ac154eb6d33fd1d6408a6954617ab56e0
Cheng-mean Liu <soccerl@microsoft.com>
2017-11-27 23:27:12 +0000

    cleaning up some scripts

--- DCOS/MSI/client/net.ps1
+++ DCOS/MSI/client/net.ps1
@@ -7,3 +7,10 @@ New-NetIPAddress -InterfaceIndex $ifIndex -IPAddress 169.254.169.254
 Write-host "wait for the network setting to be ready for use..."
 Start-Sleep -s 4
 ipconfig
+
+#reading IMSProxyIpAddress
+$x = "IMSProxyIpAddress"
+$IMSProxyIpAddress = (get-item env:$x).Value
+
+Write-Host "IMSProxyIpAddress = $IMSProxyIpAddress"
+Netsh interface portproxy add v4tov4 listenaddress=169.254.169.254 listenport=80 connectaddress=$IMSProxyIpAddress connectport=80  protocol=tcp
--- DCOS/MSI/client/setup.bat
+++ DCOS/MSI/client/setup.bat
@@ -1,5 +1,3 @@
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\net.ps1'"
-echo %IMSProxyIpAddress%
-Netsh interface portproxy add v4tov4 listenaddress=169.254.169.254 listenport=80 connectaddress=%IMSProxyIpAddress% connectport=80  protocol=tcp
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& Invoke-WebRequest -Uri "http://169.254.169.254" -Method GET -UseBasicParsing"
 cmd
