5e2a2e65a7a3183de7cdc0dc2a9c63a6e2b01637
Cheng-mean Liu <soccerl@microsoft.com>
2017-11-27 19:08:01 +0000

    Passed IMSProxyIpAddress via environment variable during the docker run

--- DCOS/MSI/client/networksetup.ps1
+++
@@ -1,8 +0,0 @@
-
-
-$interfaceName = get-netadapter | select -expand Name
-$gatewayIP = get-netroute -DestinationPrefix '0.0.0.0/0' | select -ExpandProperty NextHop
-netsh interface ipv4 add address "$interfaceName" 169.254.169.254 255.255.255.255 $gatewayIP
-#Netsh interface portproxy add v4tov4 listenaddress=169.25.169.254 listenport=80 connectaddress=172.24.38.149 connectport=80  protocol=tcp
-powershell
-
--- DCOS/MSI/client/setup.bat
+++ DCOS/MSI/client/setup.bat
@@ -1,4 +1,5 @@
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\net.ps1'"
-Netsh interface portproxy add v4tov4 listenaddress=169.254.169.254 listenport=80 connectaddress=172.24.38.149 connectport=80  protocol=tcp
+echo %IMSProxyIpAddress%
+Netsh interface portproxy add v4tov4 listenaddress=169.254.169.254 listenport=80 connectaddress=%IMSProxyIpAddress% connectport=80  protocol=tcp
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& Invoke-WebRequest -Uri "http://169.254.169.254" -Method GET -UseBasicParsing"
 cmd
